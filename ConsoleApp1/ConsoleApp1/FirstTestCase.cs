09cfa7bcb565de172b5b0397ccd77c715322c2fe
Apiquet <anthony.piquet2@gmail.com>
2018-02-03 00:21:00 -0800

    Set up VS and Selenium

---
+++ ConsoleApp1/ConsoleApp1/FirstTestCase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace ConsoleApp1
+{
+    class FirstTestCase
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
