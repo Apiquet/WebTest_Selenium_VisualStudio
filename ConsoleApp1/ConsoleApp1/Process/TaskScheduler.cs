07ed8fc4e7fdb3c5a8a94d07e43f5daa5c5e266d
Apiquet <anthony.piquet2@gmail.com>
2018-02-07 23:43:11 -0800

    change namespace

--- ConsoleApp1/ConsoleApp1/Process/TaskScheduler.cs
+++ ConsoleApp1/ConsoleApp1/Process/TaskScheduler.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Microsoft.Win32.TaskScheduler;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1.Process
+namespace Webtests
 {
     public class TaskScheduler
     {
