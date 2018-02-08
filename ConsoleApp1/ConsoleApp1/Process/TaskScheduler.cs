3d9e4e253668b9073df791e7547af9b008855450
Apiquet <anthony.piquet2@gmail.com>
2018-02-07 23:46:54 -0800

    Add Task on Windows Task Scheduler

--- ConsoleApp1/ConsoleApp1/Process/TaskScheduler.cs
+++ ConsoleApp1/ConsoleApp1/Process/TaskScheduler.cs
@@ -9,6 +9,28 @@ namespace Webtests
 {
     public class TaskScheduler
     {
-        
+        public void ScheduleTask(string heure, string minute)
+        {
+            using (TaskService RegisterTask = new TaskService())
+            {
+                // Create a new task
+                TaskDefinition td = RegisterTask.NewTask();
+                td.RegistrationInfo.Description = "Fiddler runner";
+
+                // Create a trigger
+                td.Triggers.Add(new TimeTrigger
+                {
+                    StartBoundary =
+                        Convert.ToDateTime(DateTime.Today.Date.ToString("MM/dd/yyyy") + " " + heure + ":" + minute + ":00 AM")
+                });
+
+                // Create an action that will launch Fiddler in quiet mode
+                td.Actions.Add(new ExecAction("C:/Users/AppData/Local/Programs/Fiddler/Fiddler.exe",
+                    "/quiet", null));
+
+                // Register the task in the root folder
+                RegisterTask.RootFolder.RegisterTaskDefinition(@"CScheduler\FiddlerRunner", td);
+            }
+        }
     }
 }
